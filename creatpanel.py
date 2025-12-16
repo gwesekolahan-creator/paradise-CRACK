27d114548bcbc0b87895865de2781c07d95398b1
gwesekolahan-creator <gwesekolahan@gmail.com>
2025-12-16 12:20:37 +0700

    Update creatpanel.py

--- creatpanel.py
+++ creatpanel.py
@@ -5,7 +5,7 @@ from faker import Faker
 import textwrap
 
 TOTAL_COOKIES = 1
-TOTAL_CP = 1
+TOTAL_CP = 0
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
@@ -376,6 +376,7 @@ def confirm(uid, mail, otp, ses):
 # =========================
 if __name__ == "__main__":
     main()
+
 
 
 
